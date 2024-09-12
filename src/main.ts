869219312d156699104b87d085409ceed9814d09
RexBearIU <rexbear87941@gmail.com>
2024-09-12 13:11:31 +0800

    feat:delete unneed logger

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  this.logger.log(`auto-trade is running on: ${await app.getUrl()}`);
 }
 bootstrap();
